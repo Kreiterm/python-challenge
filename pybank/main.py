ed159f13d0666bc58c72982479406dcb4e767166
Kreiterm <78062848+Kreiterm@users.noreply.github.com>
2021-02-22 21:39:55 -0500

    Added in my import functions and resource folders

--- pybank/main.py
+++ pybank/main.py
@@ -1,2 +1,19 @@
+
+import os
+import csv
+
+output_file = '../documents/Github/python-challenge/pybank/Resources/budget_data.csv'
+
 # REMEMBER TO DOUBLE CHECK WHICH ASSIGNMENT THIS IS BEFORE I CODE!
 # Don't freak out, I can handle this. I can get this done. I can do this.
+
+# Open and read csv
+with open(output_file) as budget_data:
+    csv_reader = csv.reader(budget_data, delimiter=",")
+
+    # Read the header row first (skip this part if there is no header)
+    csv_header = next(csv_reader)
+    #print(f"Header: {csv_header}")
+
+    # Read through each row of data after the header
+    for row in csv_reader:
--- pypoll/main.py
+++ pypoll/main.py
@@ -1,2 +1,19 @@
+
+import os
+import csv
+
+output_file = '../documents/Github/python-challenge/pypoll/Resources/election_data.csv'
+
 # REMEMBER TO DOUBLE CHECK WHICH ASSIGNMENT THIS IS BEFORE I CODE!
 # Don't freak out, I can handle this. I can get this done. I can do this.
+
+# Open and read csv
+with open(output_file) as election_data:
+    csv_reader = csv.reader(election_data, delimiter=",")
+
+    # Read the header row first (skip this part if there is no header)
+    csv_header = next(csv_reader)
+    #print(f"Header: {csv_header}")
+
+    # Read through each row of data after the header
+    for row in csv_reader:
